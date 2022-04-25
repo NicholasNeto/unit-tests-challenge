d20eecd1b4f2c910257c810f57961a294b792d49
NicholasNeto <nicholasneto@outlook.com>
2022-04-24 21:20:26 -0300

    add test for user profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,56 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+
+// let authenticateUserUseCase: AuthenticateUserUseCase;
+
+// let user : ICreateUserDTO
+
+let showUserProfileUseCase: ShowUserProfileUseCase
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let user: ICreateUserDTO
+
+
+
+describe("User Profile", () => {
+
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository()
+        showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository)
+        createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+        // authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
+
+        user = {
+            name: "User Teste",
+            password: "1234",
+            email: "user@teste.com",
+        }
+    })
+
+
+    it('should be able to get a user profile', async () => {
+
+        const { id } = await createUserUseCase.execute(user)
+        if (id) {
+            const profile = await showUserProfileUseCase.execute(id)
+            expect(profile).toHaveProperty("id")
+            expect(profile).toHaveProperty("name")
+            expect(profile).toHaveProperty("email")
+        }
+    })
+
+
+    it('should not be able to get a user profile with id not exists', async () => {
+
+        expect(async () => {
+
+            await showUserProfileUseCase.execute("not exsits")
+
+        }).rejects.toBeInstanceOf(AppError)
+
+    })
+})
